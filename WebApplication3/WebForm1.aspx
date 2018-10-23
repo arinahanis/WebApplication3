b1e875abe98e6a120588970ed73d7be45be9c812
arinahanis <arinahanis97@yahoo.com>
2018-10-23 16:42:04 +0800

    aq x tahu

---
+++ WebApplication3/WebForm1.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication3.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            height: 133px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="auto-style1">
+            Enter a value<br />
+            <asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+            &nbsp;<br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication3/WebForm1.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            bool IsNumeric(string s)
+            {
+                double Result;
+                return double.TryParse(s, out Result);  // TryParse routines were added in Framework version 2.0.
+            }
+
+            string value = "123";
+            if (IsNumeric(value))
+            {
+                float sumOfSeries(double num)
+                {
+                    float res = 0, fact = 1;
+                    for (int i = 1; i <= num; i++)
+                    {
+                        // fact variable store  
+                        // factorial of the i 
+                        fact = fact * i;
+
+                        res = res + (i / fact);
+                    }
+                    return (res);
+                    Response.Write("sum" + res);
+                }
+            }
+            else
+            {
+                Response.Write("The value entered not a number");
+            }
+                
+            }
+        }
+    }
+
+                
+            
+            
+            
+        
+
+     
+    
+
